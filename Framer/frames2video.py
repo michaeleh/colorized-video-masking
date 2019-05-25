78dc5c28733bac1707d1fbc680ff0ddc2c3b1208
מיכאל <mitk13@gmail.com>
2019-05-25 18:49:24 +0300

    b&w support

--- Framer/frames2video.py
+++ Framer/frames2video.py
@@ -8,7 +8,7 @@ video_name = 'bw_mask.avi'
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
-
+# 30 fps
 video = cv2.VideoWriter(video_name, 0, 30, (width, height))
 i = 0
 for image in images:
