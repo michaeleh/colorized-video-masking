5dbf631f2a042efb217719b34e74272bf19f8a91
מיכאל <mitk13@gmail.com>
2019-05-25 13:30:35 +0300

    changes

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ from Framer.video2frames import extract_frames
 from Mask_RCNN.api.img_detector import Detector
 
 # from video path
-base_dir = "C:/michael/work/Practice/Drone/RGB/"
+base_dir = "./Drone/RGB/"
 paths = os.listdir(base_dir)
 for movie_path in paths:
-    frames = extract_frames(base_dir + movie_path, max_frames=5, use_grayscale=True)
+    frames = extract_frames(base_dir + movie_path, max_frames=5, use_grayscale=False)
     # mask frames
     detector = Detector()
     detector.detect_images(frames)
